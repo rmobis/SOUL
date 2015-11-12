c197ef69969ab1ef870fa6c5fddff8c237fc8bcb
Lucas Schanner <lsschanner@gmail.com>
2015-11-12 16:51:37 -0200

    mais duas syscalls feitas (porem não testadas)

--- soul.s
+++ soul.s
@@ -1,3 +1,4 @@
+@ vim:ft=armv5
 .set TIME_SZ 200000
 .org 0x0
 .section .iv,"a"
@@ -137,7 +138,7 @@ read_sonar:
     ldr r1, [r2]
 
     bic r1, r1, #0x0000003C @ Máscara para os bits do SONAR_MUX
-    mov r0, (r0 << 2)
+    lsl r0, r0, #2
     and r0, r0, #0x0000003C
     eor r1, r1, r0
     
@@ -207,9 +208,73 @@ fim:
     ldmfd sp, {r1-r10}
     movs pc, lr
     
+@ /read_sonar
+
+set_motor_speed:
+    cmp r1, #63
+    bhi quit
+    cmp r0, #0
+    beq set_motor0
+    cmp r0, #1
+    beq set_motor1
+    mov r0, #-1
+    movs pc, lr
+quit:
+    mov r0, #-2
+    movs pc, lr
 
 
+set_motor0:
+    stmfd sp, {r1-r10}
+
+    mov r2, #0x53F84000 @ Endereço do registrador de dados da GPIO
+    ldr r3, [r2]
+
+    orr r3, r3, #0x00040000 @ Escreve 1 no bit de write
+    bic r3, r3, #0x01F80000
+    lsl r1, r1, #19
+    eor r3, r3, r1
     
+    str r3, [r2]
+
+    bic r3, r3, #0x00040000 @ Escreve 0 no bit de write
+    
+    str r3, [r2]
+
+    mov r0, #0
+
+    ldmfd sp, {r1-r10}
+    movs pc, lr
+
+set_motor1:
+    stmfd sp, {r1-r10}
+
+    mov r2, #0x53F84000 @ Endereço do registrador de dados da GPIO
+    ldr r3, [r2]
+
+    orr r3, r3, #0x02000000 @ Escreve 1 no bit de write
+    bic r3, r3, #0xFC000000
+    lsl r1, r1, #26
+    eor r3, r3, r1
+    
+    str r3, [r2]
+
+    bic r3, r3, #0x02000000 @ Escreve 0 no bit de write
+    
+    str r3, [r2]
+
+    mov r0, #0
+
+    ldmfd sp, {r1-r10}
+    movs pc, lr
+
+@/set_motor_speed
+
+get_time:
+    mov r0, SYS_TIME
+    ldr r0, [r0]
+
+    movs pc, lr
 
 .data
 SYS_TIME:
